debfcdbec6b2dae36785cfe0c82ad50bffd6d27c
glasses-png <cchelangat16@gmail.com>
2020-05-24 16:19:24 +0300

    add models,superuser

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns=[
   url(r'^$', views.index, name='index'),
   url(r'^search/', views.search_results, name='search'),
-  url(r'^location/(?P<location>\w+', views.image_location, name='location'),
+  url(r'^location/', views.image_location, name='location'),
 ]
